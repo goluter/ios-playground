5f390eb6bed46e22e9a8874c11a6560eaa9dd455
kang juyoung <201511080@sangmyung.kr>
2020-10-02 15:13:13 +0900

    Update base project

---
+++ playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playground
+//
+//  Created by kang juyoung on 2020/10/02.
+//  Copyright © 2020 JooYoung Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
