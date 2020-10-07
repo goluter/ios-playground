1daee0860b046cc0958d9a10d1739727df87b9ab
Ben (Joo Young Kang) <jy.kang@ecubelabs.com>
2020-10-07 17:49:20 +0900

    Pod init

---
+++ Pods/Target Support Files/Pods-ios-playground-ios-playgroundUITests/Pods-ios-playground-ios-playgroundUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_playground_ios_playgroundUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ios_playground_ios_playgroundUITests
+@end
